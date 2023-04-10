a27c9df10b5dd5856fe12fa8bea8e7610faf0e9b
Efua Yankey <efuayankey2003@gmail.com>
2023-04-10 03:57:22 -0700

    bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
 void print_binary(unsigned long int n)
 {
 
-int i' j;
+int i, j;
 
 if (n == 0)
 {
@@ -21,8 +21,8 @@ for (i = 63; i >= 0; i--)
 
 	if (j & 1)
 		_putchar('1');
-	else if (i == 0);
-	_putchar('0');
+	else
+		_putchar('0');
 }
 
 }
