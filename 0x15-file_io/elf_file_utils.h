c183e1ac32de55e571ea06d37ff7ef93e3ca8339
Efua Yankey <efuayankey2003@gmail.com>
2023-05-08 20:03:06 -0700

    File I/O Project

---
+++ 0x15-file_io/elf_file_utils.h
@@ -0,0 +1,18 @@
+#ifndef ELF_FILE_UTILS.H
+#define ELF_FILE_UTILS.H
+
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+#endif
---
+++ 0x15-file_io/elf_info_printer.h
@@ -0,0 +1,18 @@
+#ifndef ELF_INFO_PRINTER_H
+#define ELF_INFO_PRINTER_H
+
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+
+#endif
